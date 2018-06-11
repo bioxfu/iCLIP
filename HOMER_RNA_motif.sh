0de210caea1e1a54f541e2951aab5c1af4f65654
Fu Xing <bio.xfu@gmail.com>
2018-06-11 16:02:31 +0800

    update

--- HOMER_RNA_motif.sh
+++ HOMER_RNA_motif.sh
@@ -2,4 +2,4 @@ mkdir homer
 
 find sites/*.bed|grep -v 'ctrl'|./bin/rush -k "bin/sites2windows.sh  {%@([^.]+)\.}"
 
-find homer/* | ./bin/rush -k "findMotifsGenome.pl {} mm10 {@(.+).sites.*}_MotifOutput -rna -bg mm10_RefSeq_gene.bed -len 6"
+find homer/* | ./bin/rush -k "findMotifsGenome.pl {} mm10 {@(.+).sites.*}_MotifOutput -rna -bg bg/mm10_RefSeq_gene.bed -len 6"
