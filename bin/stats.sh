82f4bbc2849055d762f9cdf2274c69a7fc0a7cda
Fu Xing <bio.xfu@gmail.com>
2018-09-28 09:02:15 +0800

    support samples without replicates

--- bin/stats.sh
+++ bin/stats.sh
@@ -1,5 +1,6 @@
+TYPE=$1
+
 grep 'Number of input reads' mapping/*/Log.final.out|sed -r 's/:\s+?/\t/'|sed  's/ |//'|sed 's/ /_/g'|sed 's/\/Log.final.out//'|sed 's/mapping\///' > mapping/stat.tsv
 grep 'Uniquely mapped reads number' mapping/*/Log.final.out|sed -r 's/:\s+?/\t/'|sed  's/ |//'|sed 's/ /_/g'|sed 's/\/Log.final.out//'|sed 's/mapping\///' >> mapping/stat.tsv
 
-bin/stats.R
-
+if [ $TYPE == 'no_rep' ]; then bin/stats_no_rep.R; else bin/stats.R; fi
---
+++ bin/stats_no_rep.R
@@ -0,0 +1,23 @@
+#! /usr/bin/env Rscript
+library(fastqcr)
+library(magrittr)
+
+qc_raw <- qc_aggregate('fastqc/raw') %>% qc_stats()
+qc_clean <- qc_aggregate('fastqc/trim') %>% qc_stats()
+qc <- cbind(qc_raw, qc_clean)
+qc <- qc[, c(1,4,9)]
+colnames(qc) <- c('sample', 'total.reads', 'trimed.adapter.reads')
+
+stat_tab <- matrix(nrow=length(qc$sample), ncol=5)
+rownames(stat_tab) <- qc$sample
+colnames(stat_tab) <- c('PCR.dedupliated.reads', 'uniquely.mapped.reads', 'crosslink.sites', 'crosslink.sites.FDR<0.05', 'crosslink.sites.clusters')
+mapping_stat <- read.table('mapping/stat.tsv', stringsAsFactors = F)
+
+for (i in 1:length(qc$sample)) {
+  x <- read.table(paste0('xlsites/', qc$sample[i], '_reads_unique.bed'))
+  y <- read.table(paste0('peaks/', qc$sample[i], '_reads_unique_peaks.bed'))
+  z <- read.table(paste0('peaks/', qc$sample[i], '_reads_unique_peaks_clusters.bed'))
+  stat_tab[i, ] <- c(mapping_stat[mapping_stat$V1 == qc$sample[i], 3], nrow(x), nrow(y), z[nrow(z),7])
+}
+
+write.table(cbind(qc, stat_tab), 'table/stats_tab.tsv', row.names = F, quote = F, sep = '\t')
