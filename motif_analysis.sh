4945cb1801e0bda3591026f4c01bd12b8c30677c
Fu Xing <bio.xfu@gmail.com>
2018-09-28 12:15:42 +0800

    test 10 times

--- motif_analysis.sh
+++ motif_analysis.sh
@@ -1,2 +1,11 @@
-bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window Mecp2
-bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 21nt.window Mecp2
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test1
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test2
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test3
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test4
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test5
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test6
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test7
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test8
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test9
+bin/iCLIP_jellyfish_kmerCounting.sh merge/merge_ALL_crosslink_sites_sig.bed 6 11nt.window test10
+
