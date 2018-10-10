370887f108c54dd7c471cd05ad4faaed40982ea3
Fu Xing <bio.xfu@gmail.com>
2018-10-10 09:08:16 +0800

    Kmer zscore comparision

--- motif_analysis2.R
+++ motif_analysis2.R
@@ -1,7 +1,8 @@
-#! /usr/bin/env Rscript
+library(RColorBrewer)
+cols <- brewer.pal(n = 3, 'Set1')
 
-cal_zscore <- function(dumps) {
-  kmer <- read.table('merge/crosslink.window.5mer.list')[,1]
+cal_zscore <- function(dumps, N) {
+  kmer <- read.table('merge/crosslink.window.5mer.list', stringsAsFactors = F)[,1]
   x <- matrix(0, nrow=length(kmer), ncol=101)
   rownames(x) <- kmer
   colnames(x) <- 0:100
@@ -9,7 +10,7 @@ cal_zscore <- function(dumps) {
   input <- read.table(dumps, stringsAsFactors=F)
   x[input[, 1], 1] <- input[, 2]
   
-  files <- list.files('merge/random', '*.dump', full.names = T)
+  files <- list.files(paste0('merge/random', N), '*.dump', full.names = T)
   for (i in 1:length(files)) {
     input <- read.table(files[i], stringsAsFactors=F)
     x[input[, 1], i+1] <- input[, 2]
@@ -21,20 +22,36 @@ cal_zscore <- function(dumps) {
   return(z_score)
 }
 
-
-KO_zscore <- cal_zscore('merge/KO_crosslink_sites_intron_filt_motif_50K_window_5mer.dumps')
-WT_zscore <- cal_zscore('merge/WT_crosslink_sites_intron_filt_motif_50K_window_5mer.dumps')
-colnames(KO_zscore) <- c('KO_zscore', 'KO_zscore_rank')
-colnames(WT_zscore) <- c('WT_zscore', 'WT_zscore_rank')
-
-all <- merge(WT_zscore, KO_zscore, by.x = 0, by.y = 0)
-all <- all[order(all$WT_zscore_rank), ]
-all$rank_diff <- all$WT_zscore_rank - all$KO_zscore_rank
-colnames(all)[1] <- 'motif'
-
-motifs <- read.table('motif_from_HJY')
-colnames(motifs) <- c('motif', 'class')
-motifs <- merge(motifs, all, by.x=1, by.y=1, all.x = T)
-motifs <- motifs[order(motifs$WT_zscore_rank),]
-write.table(all, 'motif2/zscore_all.tsv', quote=F, sep='\t', row.names=F)
-write.table(motifs, 'motif2/zscore_motif_from_HJY.tsv', quote=F, sep='\t', row.names=F)
+for (N in 1:10) {
+  
+  zscore1 <- cal_zscore('merge/WT_crosslink_sites_exc_KO_intron_filt_motif_25K_window_5mer.dumps', N)
+  zscore2 <- cal_zscore('merge/KO_crosslink_sites_exc_WT_intron_filt_motif_25K_window_5mer.dumps', N)
+  colnames(zscore1) <- c('WT_specific.zscore', 'WT_specific.rank')
+  colnames(zscore2) <- c('KO_specific.zscore', 'KO_specific.rank')
+  
+  all <- merge(zscore1, zscore2, by.x = 0, by.y = 0)
+  colnames(all)[1] <- 'motif'
+  all <- all[order(all$WT_specific.rank), ]
+  all$rank_diff <- all$WT_specific.rank - all$KO_specific.rank
+  
+  motifs <- read.table('motif_hnRNP')
+  colnames(motifs) <- c('motif', 'class')
+  motifs <- merge(motifs, all, by.x=1, by.y=1, all.x = T)
+  motifs <- motifs[order(motifs$WT_specific.rank),]
+  write.table(all, paste0('merge/zscore_all_random', N, '.tsv'), quote=F, sep='\t', row.names=F)
+  write.table(motifs, paste0('merge/zscore_motif_hnRNP_random', N, '.tsv'), quote=F, sep='\t', row.names=F)
+  
+  motifs <- motifs[-c(1:2),]
+  x <- sort(motifs$rank_diff)
+  n <- motifs$motif[order(motifs$rank_diff)]
+  y <- as.character(motifs$class[order(motifs$rank_diff)])
+  y[y=='green'] <- cols[3]
+  y[y=='blue'] <- cols[2]
+  
+  pdf(paste0('figure/zscore_motif_hnRNP_random', N, '.pdf'))
+  barplot(x, names.arg = n, las=2, col=y, border = NA, main='Z-score rank difference of hnRNP motifs', ylab='WT_specific - KO_specific', space = 0.5, ylim=c(-140, 40), yaxt='n')
+  axis(2, at=seq(-140, 40, 20), lab=seq(-140, 40, 20))
+  legend('bottomright', c('hnRNP M motifs', 'hnRNP C motifs', 'hnRNP H motifs'), fill = c(cols[2], cols[3], 'gray'), bty='n', border = NA)
+  dev.off()
+  
+}
