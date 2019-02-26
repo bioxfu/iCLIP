62df429884a7d579361abee5d374036aa7f4913e
Fu Xing <bio.xfu@gmail.com>
2019-02-26 15:12:26 +0800

    update

--- motif_analysis2.R
+++ motif_analysis2.R
@@ -1,8 +1,8 @@
 library(RColorBrewer)
 cols <- brewer.pal(n = 3, 'Set1')
 
-cal_zscore <- function(dumps, N) {
-  kmer <- read.table('merge/crosslink.window.5mer.list', stringsAsFactors = F)[,1]
+cal_zscore <- function(dumps, N, K) {
+  kmer <- read.table(paste0('merge/crosslink.window.', K, '.list'), stringsAsFactors = F)[,1]
   x <- matrix(0, nrow=length(kmer), ncol=101)
   rownames(x) <- kmer
   colnames(x) <- 0:100
@@ -10,7 +10,7 @@ cal_zscore <- function(dumps, N) {
   input <- read.table(dumps, stringsAsFactors=F)
   x[input[, 1], 1] <- input[, 2]
   
-  files <- list.files(paste0('merge/random', N), '*.dump', full.names = T)
+  files <- list.files(paste0('merge/', K, '/random', N), '*.dump', full.names = T)
   for (i in 1:length(files)) {
     input <- read.table(files[i], stringsAsFactors=F)
     x[input[, 1], i+1] <- input[, 2]
@@ -22,10 +22,12 @@ cal_zscore <- function(dumps, N) {
   return(z_score)
 }
 
+K <- '6mer'
+
 for (N in 1:10) {
   
-  zscore1 <- cal_zscore('merge/WT_crosslink_sites_exc_KO_intron_filt_motif_50K_window_5mer.dumps', N)
-  zscore2 <- cal_zscore('merge/KO_crosslink_sites_exc_WT_intron_filt_motif_50K_window_5mer.dumps', N)
+  zscore1 <- cal_zscore(paste0('merge/WT_crosslink_sites_exc_KO_intron_filt_motif_50K_window_', K, '.dumps'), N, K)
+  zscore2 <- cal_zscore(paste0('merge/KO_crosslink_sites_exc_WT_intron_filt_motif_50K_window_', K, '.dumps'), N, K)
   colnames(zscore1) <- c('WT_specific.zscore', 'WT_specific.rank')
   colnames(zscore2) <- c('KO_specific.zscore', 'KO_specific.rank')
   
@@ -34,24 +36,28 @@ for (N in 1:10) {
   all <- all[order(all$WT_specific.rank), ]
   all$rank_diff <- all$WT_specific.rank - all$KO_specific.rank
   
-  motifs <- read.table('motif_hnRNP')
-  colnames(motifs) <- c('motif', 'class')
-  motifs <- merge(motifs, all, by.x=1, by.y=1, all.x = T)
-  motifs <- motifs[order(motifs$WT_specific.rank),]
-  write.table(all, paste0('merge/zscore_all_random', N, '.tsv'), quote=F, sep='\t', row.names=F)
-  write.table(motifs, paste0('merge/zscore_motif_hnRNP_random', N, '.tsv'), quote=F, sep='\t', row.names=F)
-  
-  motifs <- motifs[motifs$class != 'Fox',]
-  x <- sort(motifs$rank_diff)
-  n <- motifs$motif[order(motifs$rank_diff)]
-  y <- as.character(motifs$class[order(motifs$rank_diff)])
-  y[y=='green'] <- cols[3]
-  y[y=='blue'] <- cols[2]
-  
-  pdf(paste0('figure/zscore_motif_hnRNP_random', N, '.pdf'))
-  barplot(x, names.arg = n, las=2, col=y, border = NA, main='Z-score rank difference of hnRNP motifs', ylab='WT_specific - KO_specific', space = 0.5, ylim=c(-320, 80), yaxt='n')
-  axis(2, at=seq(-320, 80, 40), lab=seq(-320, 80, 40))
-  legend('bottomright', c('hnRNP M motifs', 'hnRNP C motifs', 'hnRNP H motifs'), fill = c(cols[2], cols[3], 'gray'), bty='n', border = NA)
-  dev.off()
-  
+  if (K == '5mer') {
+    motifs <- read.table('motif_hnRNP')
+    colnames(motifs) <- c('motif', 'class')
+    motifs <- merge(motifs, all, by.x=1, by.y=1, all.x = T)
+    motifs <- motifs[order(motifs$WT_specific.rank),]
+    write.table(all, paste0('merge/', K, '/zscore_all_random', N, '.tsv'), quote=F, sep='\t', row.names=F)
+    write.table(motifs, paste0('merge/', K, '/zscore_motif_hnRNP_random', N, '.tsv'), quote=F, sep='\t', row.names=F)
+    
+    motifs <- motifs[motifs$class != 'Fox',]
+    x <- sort(motifs$rank_diff)
+    n <- motifs$motif[order(motifs$rank_diff)]
+    y <- as.character(motifs$class[order(motifs$rank_diff)])
+    y[y=='green'] <- cols[3]
+    y[y=='blue'] <- cols[2]
+    
+    pdf(paste0('figure/', K, '_zscore_motif_hnRNP_random', N, '.pdf'))
+    barplot(x, names.arg = n, las=2, col=y, border = NA, main='Z-score rank difference of hnRNP motifs', ylab='WT_specific - KO_specific', space = 0.5, ylim=c(-320, 80), yaxt='n')
+    axis(2, at=seq(-320, 80, 40), lab=seq(-320, 80, 40))
+    legend('bottomright', c('hnRNP M motifs', 'hnRNP C motifs', 'hnRNP H motifs'), fill = c(cols[2], cols[3], 'gray'), bty='n', border = NA)
+    dev.off()
+  }
+  else {
+    write.table(all, paste0('merge/', K, '/zscore_all_random', N, '.tsv'), quote=F, sep='\t', row.names=F)
+  }
 }
--- motif_analysis2.sh
+++ motif_analysis2.sh
@@ -6,7 +6,7 @@ GENOME=$HOME/Gmatic7/iCount/homo_sapiens.88.fa
 GENOMESIZE=$HOME/Gmatic7/iCount/homo_sapiens.88.chrLength
 INTRON=$HOME/Gmatic7/iCount/homo_sapiens.88.intron.bed
 CPU=8
-K=5
+K=6
 
 # exclude WT sites from KO sites
 bedtools window -a merge/merge_ALL_crosslink_sites_sig_clusters_KO.bed -b merge/merge_ALL_crosslink_sites_sig_clusters_WT.bed -w 40 -v > merge/KO_crosslink_sites_exc_WT.bed
@@ -36,24 +36,25 @@ jellyfish count -m $K -s 100M -t $CPU merge/WT_crosslink_sites_exc_KO_intron_fil
 jellyfish dump merge/KO_crosslink_sites_exc_WT_intron_filt_motif_50K_window_${K}mer.jf_0 -c -t|sort > merge/KO_crosslink_sites_exc_WT_intron_filt_motif_50K_window_${K}mer.dumps
 jellyfish dump merge/WT_crosslink_sites_exc_KO_intron_filt_motif_50K_window_${K}mer.jf_0 -c -t|sort > merge/WT_crosslink_sites_exc_KO_intron_filt_motif_50K_window_${K}mer.dumps
 
-cat merge/*.dumps|cut -f1|sort|uniq > merge/crosslink.window.${K}mer.list
+cat merge/*${K}mer.dumps|cut -f1|sort|uniq > merge/crosslink.window.${K}mer.list
 
 rm merge/*.jf_0
 
 ## generate 10 random background
+mkdir merge/${K}mer
 for N in {1..10}
 do
-mkdir merge/random
+mkdir merge/${K}mer/random
 for i in {1..100}
 do
 	echo $i
-    bedtools shuffle -i merge/KO_crosslink_sites_exc_WT_intron_filt_motif_50K_window.bed -g $GENOMESIZE -incl $INTRON > merge/random/random.$i.bed
-    bedtools getfasta -fi $GENOME -bed merge/random/random.$i.bed -name -s -fo merge/random/random.$i.fa
-    jellyfish count -m $K -s 100M merge/random/random.$i.fa -o merge/random/random.${K}mer.$i.jf
-    jellyfish dump merge/random/random.${K}mer.$i.jf_0 -c -t |sort > merge/random/random.${K}mer.$i.dumps
-	rm merge/random/random.$i.bed merge/random/random.$i.fa merge/random/random.${K}mer.$i.jf_0
+    bedtools shuffle -i merge/KO_crosslink_sites_exc_WT_intron_filt_motif_50K_window.bed -g $GENOMESIZE -incl $INTRON > merge/${K}mer/random/random.$i.bed
+    bedtools getfasta -fi $GENOME -bed merge/${K}mer/random/random.$i.bed -name -s -fo merge/${K}mer/random/random.$i.fa
+    jellyfish count -m $K -s 100M merge/${K}mer/random/random.$i.fa -o merge/${K}mer/random/random.${K}mer.$i.jf
+    jellyfish dump merge/${K}mer/random/random.${K}mer.$i.jf_0 -c -t |sort > merge/${K}mer/random/random.${K}mer.$i.dumps
+	rm merge/${K}mer/random/random.$i.bed merge/${K}mer/random/random.$i.fa merge/${K}mer/random/random.${K}mer.$i.jf_0
 done
-mv merge/random merge/random${N}
+mv merge/${K}mer/random merge/${K}mer/random${N}
 done
 
 
