557aa737b049583dd3b647d3768a4d107ef3bf65
Fu Xing <bio.xfu@gmail.com>
2019-10-18 14:07:45 +0800

    update RBP occupancy

--- RBP_occupancy_norm.sh
+++ RBP_occupancy_norm.sh
@@ -4,6 +4,7 @@ mkdir occupancy
 
 #chr10|+|101232253|101232434|101229610|101229814|101237938|101238237	enhanced
 
+## link the crosslink sites with genes
 bedtools intersect -a xlsites/MeCP2_KO_rep1.TGGTCA_reads_unique.bed -b $GENE -wa -wb -s|awk '{print $1"\t"$2"\t"$3"\t"$10"\t"$5"\t"$6}'|groupBy -g 1,2,3 -c 4 -o collapse -full|grep -v ','|cut -f1-6 > occupancy/MeCP2_KO_rep1.bed
 bedtools intersect -a xlsites/MeCP2_KO_rep2.CACTGT_reads_unique.bed -b $GENE -wa -wb -s|awk '{print $1"\t"$2"\t"$3"\t"$10"\t"$5"\t"$6}'|groupBy -g 1,2,3 -c 4 -o collapse -full|grep -v ','|cut -f1-6 > occupancy/MeCP2_KO_rep2.bed
 bedtools intersect -a xlsites/MeCP2_KO_rep3.ATTGGC_reads_unique.bed -b $GENE -wa -wb -s|awk '{print $1"\t"$2"\t"$3"\t"$10"\t"$5"\t"$6}'|groupBy -g 1,2,3 -c 4 -o collapse -full|grep -v ','|cut -f1-6 > occupancy/MeCP2_KO_rep3.bed
@@ -12,10 +13,11 @@ bedtools intersect -a xlsites/MeCP2_WT_rep1.CGTGAT_reads_unique.bed -b $GENE -wa
 bedtools intersect -a xlsites/MeCP2_WT_rep2.ACATCG_reads_unique.bed -b $GENE -wa -wb -s|awk '{print $1"\t"$2"\t"$3"\t"$10"\t"$5"\t"$6}'|groupBy -g 1,2,3 -c 4 -o collapse -full|grep -v ','|cut -f1-6 > occupancy/MeCP2_WT_rep2.bed
 bedtools intersect -a xlsites/MeCP2_WT_rep3.GCCTAA_reads_unique.bed -b $GENE -wa -wb -s|awk '{print $1"\t"$2"\t"$3"\t"$10"\t"$5"\t"$6}'|groupBy -g 1,2,3 -c 4 -o collapse -full|grep -v ','|cut -f1-6 > occupancy/MeCP2_WT_rep3.bed
 
+## merge the three replicates
 cat occupancy/MeCP2_KO_rep*.bed|sortBed|groupBy -g 1,2,3,4,6 -c 5 -o sum |awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$6"\t"$5}' > occupancy/merge_MeCP2_KO.bed
 cat occupancy/MeCP2_WT_rep*.bed|sortBed|groupBy -g 1,2,3,4,6 -c 5 -o sum |awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$6"\t"$5}' > occupancy/merge_MeCP2_WT.bed
 
-
+## 5' and 3' splicing site of silined/enhanced exon
 grep '+' MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhance_silence|grep 'enhanced'|cut -f1|sed 's/chr//'|tr '|' '\t'|sort|uniq|awk '{print $1"\t"$5-100"\t"$5+50"\t.\t.\t"$2}' >  MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS.bed
 grep '-' MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhance_silence|grep 'enhanced'|cut -f1|sed 's/chr//'|tr '|' '\t'|sort|uniq|awk '{print $1"\t"$6-50"\t"$6+100"\t.\t.\t"$2}' >> MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS.bed
 grep '+' MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhance_silence|grep 'silenced'|cut -f1|sed 's/chr//'|tr '|' '\t'|sort|uniq|awk '{print $1"\t"$5-100"\t"$5+50"\t.\t.\t"$2}' >  MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_upstream_3SS.bed
@@ -26,10 +28,11 @@ grep '+' MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhance_silence|grep 'silenced'|cut -f1|se
 grep '-' MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhance_silence|grep 'silenced'|cut -f1|sed 's/chr//'|tr '|' '\t'|sort|uniq|awk '{print $1"\t"$5-100"\t"$5+50"\t.\t.\t"$2}' >> MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_downstream_5SS.bed
 
 # 1: at least 1 (>=1)
+## normalized the cDNA counts by gene length and total number of reads
 python bin/RBP_occupancy_norm.py $GENE_LEN occupancy/merge_MeCP2_KO.bed 1 > occupancy/merge_MeCP2_KO_norm.bed
 python bin/RBP_occupancy_norm.py $GENE_LEN occupancy/merge_MeCP2_WT.bed 1 > occupancy/merge_MeCP2_WT_norm.bed
 
-
+## link the normalized crosslink sites with splicing sites
 bedtools intersect -a MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS.bed -b occupancy/merge_MeCP2_KO_norm.bed -wa -wb -s > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_KO_occu
 bedtools intersect -a MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS.bed -b occupancy/merge_MeCP2_WT_norm.bed -wa -wb -s > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_WT_occu
 bedtools intersect -a MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_upstream_3SS.bed -b occupancy/merge_MeCP2_KO_norm.bed -wa -wb -s > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_upstream_3SS_KO_occu
@@ -39,7 +42,8 @@ bedtools intersect -a MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_downstream_5SS.bed -
 bedtools intersect -a MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_downstream_5SS.bed -b occupancy/merge_MeCP2_KO_norm.bed -wa -wb -s > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_downstream_5SS_KO_occu
 bedtools intersect -a MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_downstream_5SS.bed -b occupancy/merge_MeCP2_WT_norm.bed -wa -wb -s > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_downstream_5SS_WT_occu
 
-
+## calculate the distance between cross link sites and splicing sites
+## sum the normalized counts at each position
 cat occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_KO_occu |awk '{if($6=="+"){print $8-$2"\t"$11} if($6=="-"){print 149-($8-$2)"\t"$11}}'|sort -n|groupBy -g 1 -c 2 -o sum > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_KO_dist2occu
 cat occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_WT_occu |awk '{if($6=="+"){print $8-$2"\t"$11} if($6=="-"){print 149-($8-$2)"\t"$11}}'|sort -n|groupBy -g 1 -c 2 -o sum > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_WT_dist2occu
 cat occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_upstream_3SS_KO_occu |awk '{if($6=="+"){print $8-$2"\t"$11} if($6=="-"){print 149-($8-$2)"\t"$11}}'|sort -n|groupBy -g 1 -c 2 -o sum > occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_upstream_3SS_KO_dist2occu
