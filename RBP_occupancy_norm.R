73e6577025029437939f7a24e097f5fcb1c89353
Fu Xing <bio.xfu@gmail.com>
2019-10-25 18:21:20 +0800

    RBP occupancy back to origin version

--- RBP_occupancy_norm.R
+++ RBP_occupancy_norm.R
@@ -21,22 +21,20 @@ wt_si_3ss <- read.table('occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_silenced_upstrea
 ko_en_3ss <- read.table('occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_KO_dist2occu')
 wt_en_3ss <- read.table('occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_enhanced_upstream_3SS_WT_dist2occu')
 
-p_cutoff <- 0.01
-fc_cutoff <- 2
-win_size <- 20
 
 #pdf('occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_v2.pdf')
-pdf('occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_v5.pdf', hei=5)
+pdf('occupancy/MeCP2_KO_RBFOX2_KI_SE_RPKM_2_v6.pdf', hei=5)
 par(mfrow=c(2, 2))
 #par(mar=c(5,4,4,1))
 par(mar=c(2,4,1,1))
 # enhanced SE events
-plot(wt_en_3ss$V1, smooth_occu(wt_en_3ss$V2, step=10), ylim=c(0,0.8), type='l', bty='l', las=2, lwd=1.5, col=col_set[1], ylab='Normalized cDNA counts', xlab='', xaxt='n')
+plot(wt_en_3ss$V1, smooth_occu(wt_en_3ss$V2, step=10), ylim=c(0,1), type='l', bty='l', las=2, lwd=1.5, col=col_set[1], ylab='Normalized crosslink events', xlab='', xaxt='n')
 lines(ko_en_3ss$V1, smooth_occu(ko_en_3ss$V2, step=10), col=col_set[2], lwd=1.5)
 axis(1, at=c(0,50,100,150), lab=c(-100, -50, '3SS', 50))
 legend('topleft', c('WT', 'KO'), col = col_set[1:2], lwd = 3, bty='n')
 abline(h=0)
-rect(100, -0.03, 200, 0.03, col='gray30', border = NA)
+#rect(100, -0.02, 200, 0.02, col='gray', border = NA)
+rect(100, -0.03, 200, 0.03, col='black', border = NA)
 x <- smooth_occu(wt_en_3ss$V2, step=10)
 y <- smooth_occu(ko_en_3ss$V2, step=10)
 x <- c(x, x[-(1:120)])
@@ -44,20 +42,21 @@ y <- c(y, y[-(1:120)])
 p <- c()
 fc <- c()
 for (i in 1:150) {
-  p[i] <- t.test(x[i:(i+win_size)], y[i:(i+win_size)])$p.value
-  fc[i] <- log2(mean(x[i:(i+win_size)])/mean(y[i:(i+win_size)]))
+  p[i] <- t.test(x[i:(i+30)], y[i:(i+30)])$p.value
+  fc[i] <- log2(mean(x[i:(i+30)])/mean(y[i:(i+30)]))
 }
-start <- which(p.adjust(p) < p_cutoff  & fc > log2(fc_cutoff))
-end <- which(p.adjust(p) < p_cutoff & fc > log2(fc_cutoff))+win_size
-rect(min(start), 0, max(end), 10, col = alpha(col=col_set[1], 0.5), border = NA)
+start <- which(p.adjust(p) < 0.01 & fc > log2(1.5))
+end <- which(p.adjust(p) < 0.01 & fc > log2(1.5))+30
+rect(min(start), 0, max(end), 10, col = alpha(col=col_set[1], 0.1), border = NA)
 
 #par(mar=c(5,1,4,4))
 par(mar=c(2,1,1,4))
-plot(wt_en_5ss$V1, smooth_occu(wt_en_5ss$V2, step=10), ylim=c(0,0.8), type='l', bty='n', yaxt='n', las=2, lwd=1.5, col=col_set[1], ylab='', xlab='', main='', xaxt='n')
+plot(wt_en_5ss$V1, smooth_occu(wt_en_5ss$V2, step=10), ylim=c(0,1), type='l', bty='n', yaxt='n', las=2, lwd=1.5, col=col_set[1], ylab='', xlab='', main='', xaxt='n')
 lines(ko_en_5ss$V1, smooth_occu(ko_en_5ss$V2, step=10), col=col_set[2], lwd=1.5)
 axis(1, at=c(-100,0,50,100,150,200), lab=c('', -50, '5SS', 50, 100, ''))
 abline(h=0)
-rect(-50, -0.03, 50, 0.03, col='gray30', border = NA)
+#rect(-50, -0.02, 50, 0.02, col='gray', border = NA)
+rect(-50, -0.03, 50, 0.03, col='black', border = NA)
 x <- smooth_occu(wt_en_5ss$V2, step=10)
 y <- smooth_occu(ko_en_5ss$V2, step=10)
 x <- c(x, x[-(1:120)])
@@ -65,22 +64,23 @@ y <- c(y, y[-(1:120)])
 p <- c()
 fc <- c()
 for (i in 1:150) {
-  p[i] <- t.test(x[i:(i+win_size)], y[i:(i+win_size)])$p.value
-  fc[i] <- log2(mean(x[i:(i+win_size)])/mean(y[i:(i+win_size)]))
+  p[i] <- t.test(x[i:(i+30)], y[i:(i+30)])$p.value
+  fc[i] <- log2(mean(x[i:(i+30)])/mean(y[i:(i+30)]))
 }
-start <- which(p.adjust(p) < p_cutoff  & fc > log2(fc_cutoff))
-end <- which(p.adjust(p) < p_cutoff & fc > log2(fc_cutoff))+win_size
+start <- which(p.adjust(p) < 0.01 & fc > log2(1.5))
+end <- which(p.adjust(p) < 0.01 & fc > log2(1.5))+30
 rect(min(start), 0, max(end), 10, col = alpha(col=col_set[1], 0.1), border = NA)
 
 #par(mar=c(5,4,4,1))
 par(mar=c(2,4,1,1))
 # silenced SE events
-plot(wt_si_3ss$V1, smooth_occu(wt_si_3ss$V2, step=10), ylim=c(0,0.8), type='l', bty='l', las=2, lwd=1.5, col=col_set[3], ylab='Normalized cDNA counts', xlab='', xaxt='n')
+plot(wt_si_3ss$V1, smooth_occu(wt_si_3ss$V2, step=10), ylim=c(0,1), type='l', bty='l', las=2, lwd=1.5, col=col_set[3], ylab='Normalized crosslink events', xlab='', xaxt='n')
 lines(ko_si_3ss$V1, smooth_occu(ko_si_3ss$V2, step=10), col=col_set[4], lwd=1.5)
 axis(1, at=c(0,50,100,150), lab=c(-100, -50, '3SS', 50))
 legend('topleft', c('WT', 'KO'), col = col_set[3:4], lwd = 3, bty='n')
 abline(h=0)
-rect(100, -0.03, 200, 0.03, col='gray30', border = NA)
+#rect(100, -0.02, 200, 0.02, col='gray', border = NA)
+rect(100, -0.03, 200, 0.03, col='black', border = NA)
 x <- smooth_occu(wt_si_3ss$V2, step=10)
 y <- smooth_occu(ko_si_3ss$V2, step=10)
 x <- c(x, x[-(1:120)])
@@ -88,20 +88,21 @@ y <- c(y, y[-(1:120)])
 p <- c()
 fc <- c()
 for (i in 1:150) {
-  p[i] <- t.test(x[i:(i+win_size)], y[i:(i+win_size)])$p.value
-  fc[i] <- log2(mean(x[i:(i+win_size)])/mean(y[i:(i+win_size)]))
+  p[i] <- t.test(x[i:(i+30)], y[i:(i+30)])$p.value
+  fc[i] <- log2(mean(x[i:(i+30)])/mean(y[i:(i+30)]))
 }
-start <- which(p.adjust(p) < p_cutoff  & fc > log2(fc_cutoff))
-end <- which(p.adjust(p) < p_cutoff & fc > log2(fc_cutoff))+win_size
+start <- which(p.adjust(p) < 0.01 & fc > log2(1.5))
+end <- which(p.adjust(p) < 0.01 & fc > log2(1.5))+30
 rect(min(start), 0, max(end), 10, col = alpha(col=col_set[1], 0.1), border = NA)
 
 #par(mar=c(5,1,4,4))
 par(mar=c(2,1,1,4))
-plot(wt_si_5ss$V1, smooth_occu(wt_si_5ss$V2, step=10), ylim=c(0,0.8), type='l', bty='n', yaxt='n', las=2, lwd=1.5, col=col_set[3], ylab='', xlab='', main='', xaxt='n')
+plot(wt_si_5ss$V1, smooth_occu(wt_si_5ss$V2, step=10), ylim=c(0,1), type='l', bty='n', yaxt='n', las=2, lwd=1.5, col=col_set[3], ylab='', xlab='', main='', xaxt='n')
 lines(ko_si_5ss$V1, smooth_occu(ko_si_5ss$V2, step=10), col=col_set[4], lwd=1.5)
 axis(1, at=c(-100,0,50,100,150,200), lab=c('', -50, '5SS', 50, 100, ''))
 abline(h=0)
-rect(-50, -0.03, 50, 0.03, col='gray30', border = NA)
+#rect(-50, -0.02, 50, 0.02, col='gray', border = NA)
+rect(-50, -0.03, 50, 0.03, col='black', border = NA)
 x <- smooth_occu(wt_si_5ss$V2, step=10)
 y <- smooth_occu(ko_si_5ss$V2, step=10)
 x <- c(x, x[-(1:120)])
@@ -109,11 +110,11 @@ y <- c(y, y[-(1:120)])
 p <- c()
 fc <- c()
 for (i in 1:150) {
-  p[i] <- t.test(x[i:(i+win_size)], y[i:(i+win_size)])$p.value
-  fc[i] <- log2(mean(x[i:(i+win_size)])/mean(y[i:(i+win_size)]))
+  p[i] <- t.test(x[i:(i+30)], y[i:(i+30)])$p.value
+  fc[i] <- log2(mean(x[i:(i+30)])/mean(y[i:(i+30)]))
 }
-start <- which(p.adjust(p) < p_cutoff  & fc > log2(fc_cutoff))
-end <- which(p.adjust(p) < p_cutoff & fc > log2(fc_cutoff))+win_size
+start <- which(p.adjust(p) < 0.01 & fc > log2(1.5))
+end <- which(p.adjust(p) < 0.01 & fc > log2(1.5))+30
 rect(min(start), 0, max(end), 10, col = alpha(col=col_set[1], 0.1), border = NA)
 
 dev.off()
--- bin/RBP_occupancy_norm.py
+++ bin/RBP_occupancy_norm.py
@@ -1,16 +1,27 @@
 import sys
 
+lib_size = 0
 gene_sum = {}
 gene_site = {}
+gene_len = {}
 
+# gene length
 with open(sys.argv[1]) as f:
+	for line in f:
+		lst = line.strip().split('\t')
+		gene = lst[0]
+		length = int(lst[1])
+		gene_len[gene] = length / 1000
+
+with open(sys.argv[2]) as f:
 	for line in f:
 		lst = line.strip().split('\t')
 		gene = lst[3]
 		cnt = int(lst[4])
 
-		if cnt >= int(sys.argv[2]):
+		if cnt >= int(sys.argv[3]):
 			if ',' not in gene:
+				lib_size += cnt
 				if gene in gene_sum:
 					gene_sum[gene] += cnt
 					gene_site[gene] += 1
@@ -18,13 +29,16 @@ with open(sys.argv[1]) as f:
 					gene_sum[gene] = cnt
 					gene_site[gene] = 1
 
-with open(sys.argv[1]) as f:
+lib_size = lib_size / 10000000000
+
+with open(sys.argv[2]) as f:
 	for line in f:
 		lst = line.strip().split('\t')
 		gene = lst[3]
 		cnt = int(lst[4])
 
-		if cnt >= int(sys.argv[2]):
+		if cnt >= int(sys.argv[3]):
 			if ',' not in gene and gene_site[gene] > 1:
-				norm_cnt = cnt / gene_sum[gene] * 100
+				norm_cnt = cnt / gene_sum[gene] / lib_size / gene_len[gene]
+				#print("%s\t%s\t%s\t%s\t%.6f\t%s\t%s\t%s\t%s" % (lst[0], lst[1], lst[2], gene, norm_cnt, lst[5], gene_sum[gene]/gene_site[gene], lib_size, gene_len[gene]))
 				print("%s\t%s\t%s\t%s\t%.6f\t%s" % (lst[0], lst[1], lst[2], gene, norm_cnt, lst[5]))
