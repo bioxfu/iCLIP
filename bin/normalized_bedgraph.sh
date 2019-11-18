057360b0f94aac1c01e5fcd1da13d50efd545d6a
Fu Xing <bio.xfu@gmail.com>
2019-11-18 16:15:06 +0800

    add strand info in bedgraph

--- bin/normalized_bedgraph.sh
+++ bin/normalized_bedgraph.sh
@@ -3,4 +3,4 @@ OUTPUT=${INPUT}Graph
 
 N=`cat $INPUT |awk 'BEGIN{v=0}{v+=$5}END{print v/1000000}'`
 
-cat $INPUT|awk -v n="$N" '{print $1"\t"$2"\t"$3"\t"$5/n}' > $OUTPUT
+cat $INPUT|awk -v n="$N" '{if($6=="-"){$5=$5*-1} if($5!=0)print "chr"$1"\t"$2"\t"$3"\t"$5/n}' > $OUTPUT
